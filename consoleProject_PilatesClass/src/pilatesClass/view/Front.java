e9d732ce8fdb76e07065049be923d038ca34d505
504 <504@DESKTOP-ULF436B>
2023-02-28 11:48:35 +0900

    23-02-28 오전9:32 취합

--- consoleProject_PilatesClass/src/pilatesClass/view/Front.java
+++ consoleProject_PilatesClass/src/pilatesClass/view/Front.java
@@ -17,6 +17,16 @@ public class Front {
 	Scanner scanner=new Scanner(System.in);
 	
 	public void index() {
+		while (true) {
+			System.out.println("1.관리자페이지 2.회원페이지");
+			int ch = scanner.nextInt();
+			if ( ch == 1 ) { admin_page();	}
+			else if ( ch == 2 ) { member_page();	}
+		}
+		
+	}
+	
+	public void member_page() {
 		회원view 회원view=new 회원view();
 		while (true) {
 			System.out.println("1.회원가입  2.로그인");
@@ -26,5 +36,16 @@ public class Front {
 			
 		}
 	}
+	
+	public void admin_page() {
+		System.out.println("1.수업조회 2.회원조회 3.강사조회");
+		int sc = scanner.nextInt();
+		if ( sc == 1 ) {	}
+		else if ( sc == 2 ) {	}
+		else if ( sc == 3 ) {	}
+		
+	}
+	
+	
 
 }
