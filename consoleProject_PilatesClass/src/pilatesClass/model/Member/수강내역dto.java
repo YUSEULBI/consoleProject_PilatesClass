db8362febca3ec877cc84b583ac66c7dbeaf098d
YUSEULBI <ha258ppy@gmail.com>
2023-02-28 16:25:33 +0900

    2023-02-28 오후 4:25 임시 ( 취합 전 )

--- consoleProject_PilatesClass/src/pilatesClass/model/Member/수강내역dto.java
+++ consoleProject_PilatesClass/src/pilatesClass/model/Member/수강내역dto.java
@@ -2,83 +2,43 @@ package pilatesClass.model.Member;
 
 public class 수강내역dto {
 
-	
-	private int 스케줄번호;
-	private String 수강일시;
-	private int 금액;
+	private int 수강내역번호;
 	private int 회원번호_fk;
-	
-	
-	
+	private int 스케줄번호_fk;
 	
 	public 수강내역dto() {}
 
-
-
-
-	public 수강내역dto(int 스케줄번호, String 수강일시, int 금액, int 회원번호_fk) {
+	public 수강내역dto(int 수강내역번호, int 회원번호_fk, int 스케줄번호_fk) {
 		super();
-		this.스케줄번호 = 스케줄번호;
-		this.수강일시 = 수강일시;
-		this.금액 = 금액;
+		this.수강내역번호 = 수강내역번호;
 		this.회원번호_fk = 회원번호_fk;
+		this.스케줄번호_fk = 스케줄번호_fk;
 	}
 
-
-
-
-	public int get스케줄번호() {
-		return 스케줄번호;
+	public int get수강내역번호() {
+		return 수강내역번호;
 	}
 
-
-
-
-	public void set스케줄번호(int 스케줄번호) {
-		this.스케줄번호 = 스케줄번호;
+	public void set수강내역번호(int 수강내역번호) {
+		this.수강내역번호 = 수강내역번호;
 	}
-
-
-
-
-	public String get수강일시() {
-		return 수강일시;
-	}
-
-
-
-
-	public void set수강일시(String 수강일시) {
-		this.수강일시 = 수강일시;
-	}
-
-
-
-
-	public int get금액() {
-		return 금액;
-	}
-
-
-
-
-	public void set금액(int 금액) {
-		this.금액 = 금액;
-	}
-
-
-
 
 	public int get회원번호_fk() {
 		return 회원번호_fk;
 	}
 
-
-
-
 	public void set회원번호_fk(int 회원번호_fk) {
 		this.회원번호_fk = 회원번호_fk;
 	}
+
+	public int get스케줄번호_fk() {
+		return 스케줄번호_fk;
+	}
+
+	public void set스케줄번호_fk(int 스케줄번호_fk) {
+		this.스케줄번호_fk = 스케줄번호_fk;
+	}
+	
 	
 	
 	
--- consoleProject_PilatesClass/src/pilatesClass/view/스케줄View.java
+++ consoleProject_PilatesClass/src/pilatesClass/view/스케줄View.java
@@ -1,5 +1,83 @@
 package pilatesClass.view;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import pilatesClass.controller.스케줄Controller;
+import pilatesClass.model.Member.스케줄dao;
+import pilatesClass.model.Member.스케줄dto;
+
 public class 스케줄View {
 
+	private static 스케줄View view = new 스케줄View();
+	private 스케줄View() {
+		// TODO Auto-generated constructor stub
+	}
+	public static 스케줄View getInstance() {
+		return view;
+	}
+	
+	Scanner scanner = new Scanner(System.in);
+	
+	public void classPage() {
+		while(true) {
+			classView();
+			System.out.println("1.수업등록 2.수업수정 3.수업삭제");
+			int ch = scanner.nextInt();
+			if ( ch == 1 ) { classAdd();	}
+			else if ( ch == 2 ) { classEdit();	 }
+			else if ( ch == 3 ) { classDelete();	}
+		}
+	}
+	
+	public void classView() {
+		System.out.println("=========== 전체 수업목록 ===========");
+		System.out.printf("%s\t%-15s\t%s\t%s\n","수업번호","수강일시","금액","강사");
+		ArrayList<스케줄dto> classList = 스케줄Controller.getInstance().classView();
+		for ( 스케줄dto d : classList) {
+			System.out.printf("%d\t%s\t%d\t%s\n",d.get스케줄번호(),d.get수강일시(),d.get금액(),d.get강사명());
+		}
+		System.out.println("=================================");
+	}
+	
+	public 스케줄dto classAddandEditInput() {
+		System.out.print("연도 : ");	int year = scanner.nextInt();
+		System.out.print("월 : ");	int month = scanner.nextInt();
+		System.out.print("일 : ");	int day = scanner.nextInt();
+		System.out.print("시 : ");	int hour = scanner.nextInt();
+		System.out.print("분 : ");	int minute = scanner.nextInt();
+		
+		
+		System.out.print("금액 : "); int price = scanner.nextInt();
+		System.out.print("강사명 : "); String tName = scanner.next();
+		LocalDateTime time = LocalDateTime.of(year, month, day, hour, minute);
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		//2023-02-24 11:00:00
+		String stime = time.format(dtf);
+		
+		return (new 스케줄dto(0, stime, price, tName));
+		
+	}
+	
+	public void classAdd() {
+		
+		boolean result = 스케줄Controller.getInstance().classAdd(classAddandEditInput());
+		if ( result ) { System.out.println("[수업이 등록되었습니다.]");	}
+		else { System.out.println("[수업이 등록실패] - 관리자 문의");	}
+		
+	}
+	
+	public void classEdit() {
+		
+		
+	}
+	
+	public void classDelete() {
+		
+	}
+	
 }
+
