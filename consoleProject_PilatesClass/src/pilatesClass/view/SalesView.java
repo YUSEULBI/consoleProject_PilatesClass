91ab0fffe0dfabd90520071b4b3a51c4a0dc131e
YUSEULBI <ha258ppy@gmail.com>
2023-06-07 17:32:04 +0900

    6/7 오후 5시30분 월매출 다음달 선택 또는 직접입력시 이번달 이후로 조회안되도록 유효성검사

--- consoleProject_PilatesClass/src/pilatesClass/view/SalesView.java
+++ consoleProject_PilatesClass/src/pilatesClass/view/SalesView.java
@@ -72,16 +72,46 @@ public class SalesView {
 			System.out.println();
 			System.out.println("=============================================");
 			System.out.println("1.이전달 2.다음달 3.직접날짜입력 4.뒤로가기");
-			int ch = scanner.nextInt();
-			if ( ch == 1 ) { month--; if ( month<1) { year--; month=12;	} 	}
-			else if ( ch == 2 ) { month++; if ( month>12 ) { year++; month =1; }  }
-			else if ( ch == 3 ) {  	
-				System.out.println("년[yyyy] : "); year = scanner.nextInt();
-				if ( year < 1900 || year > 9999 ) { System.out.println("[연도를 맞게 입력하세요.]"); return;	}
-				System.out.println("월 : "); month = scanner.nextInt();
-				if ( month > 12 || month < 1 ) { System.out.println("[달을 맞게 입력하세요.]"); return;}
+			
+			try {
+				int ch = scanner.nextInt();
+				if ( ch == 1 ) { month--; if ( month<1) { year--; month=12;	} 	}
+				else if ( ch == 2 ) {
+					 // 유효성검사 : 입력날짜가 금월보다 많으면 안됨.
+					LocalDate currentDate = LocalDate.now();
+					LocalDate selectDate = LocalDate.of(year, month+1 , 1);
+					if( selectDate.isAfter(currentDate) ) { System.out.println("[금월까지 조회 가능합니다.]"); continue; }
+					
+					// 다음달로 넘기기
+					month++;
+					if ( month>12 ) { year++; month =1; }
+				}
+				else if ( ch == 3 ) {  	
+					LocalDate currentDate = LocalDate.now();
+					year = Calendar.getInstance().get(Calendar.YEAR); // 현재년도로 변경
+	
+					System.out.println(currentDate);
+					System.out.println("년[yyyy] : "); int selectYear = scanner.nextInt();
+					
+					// 입력년도가 현재년도보다 크거나 1900보다 낮으면 안됨.
+					if ( selectYear < 1900 || selectYear > year ) { System.err.println("[조회가능한 년도를 입력해주세요.]"); continue;	}
+					
+					// 1월보다 적거나 12월보다 많으면 안됨.
+					System.out.println("월 : "); int selectMonth = scanner.nextInt();
+					if ( month > 12 || month < 1 ) { System.err.println("[형식에 맞게 월을 입력하세요.]"); continue;}
+					
+					// 입력된 값이 지금연월보다 많으면 안됨.
+					LocalDate selectDate = LocalDate.of(selectYear, selectMonth, 1);
+					if( selectDate.isAfter(currentDate) ) { System.err.println("[금월까지 조회 가능합니다.]"); continue; }
+					
+					// 유효성검사후 선택한 연월로 변경
+					year = selectYear;
+					month = selectMonth;
+				}
+				else if ( ch == 4 ) { return; 	}
+			} catch (Exception e) {
+				System.err.println("올바른 형식으로 입력해주세요");
 			}
-			else if ( ch == 4 ) { return; 	}
 		}
 	}
 	
@@ -117,7 +147,7 @@ public class SalesView {
 			else if ( ch == 2 ) {
 				int selectday = day+1;
 				// 오늘보다 다음날로는 못가도록 - 선택한날짜가 오늘보다 많으면 안됨.
-				if ( !isPastDate(year, month, selectday) ) {  System.out.println("매출은 금일까지 조회가능합니다.");
+				if ( !isPastDate(year, month, selectday) ) {  System.err.println("[금일까지 조회가능합니다.]");
 				}else {
 					// 다음날짜구하기
 					cal.set(year, month-1, 1);
@@ -131,9 +161,9 @@ public class SalesView {
 			}// if end
 			else if ( ch == 3 ) {	
 				System.out.println("년[yyyy] : "); int selectYear = scanner.nextInt();
-				if ( selectYear < 1900 || selectYear > 9999 ) { System.out.println("[연도를 맞게 입력하세요.]"); continue;	}
+				if ( selectYear < 1900 || selectYear > 9999 ) { System.err.println("[연도를 맞게 입력하세요.]"); continue;	}
 				System.out.println("월 : "); int selectMonth = scanner.nextInt();
-				if ( selectMonth > 12 || selectMonth < 1 ) { System.out.println("[달을 맞게 입력하세요.]"); continue;}
+				if ( selectMonth > 12 || selectMonth < 1 ) { System.err.println("[달을 맞게 입력하세요.]"); continue;}
 				System.out.println("일 : "); int selectDay = scanner.nextInt();
 				
 				// 날짜가 금일 이후인지 확인
